0d6fdad2576c4401f30a6b3df911e09b9c022247
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-01-27 14:50:02 -0300

    delete: Apaguei outra referência não usada

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,7 +2,6 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { ROUTES } from '@angular/router';
-import { AppShellComponent } from './app-shell/app-shell.component';
 
 const serverConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +12,7 @@ const serverConfig: ApplicationConfig = {
         useValue: [
             {
                 path: 'shell',
-                component: AppShellComponent
+                // component:
             }
         ]
     }
