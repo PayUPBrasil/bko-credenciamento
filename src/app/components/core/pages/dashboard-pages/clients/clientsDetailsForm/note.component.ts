e769dac818cc7d8ab9200dd85ae205238870e90e
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-02-10 15:13:15 -0300

    add: commit de segurança

--- src/app/components/core/pages/dashboard-pages/clients/clientsDetailsForm/note.component.ts
+++ src/app/components/core/pages/dashboard-pages/clients/clientsDetailsForm/note.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { fadeInOut } from "../../../../../animations/fadeInAnimation.component";
-
-@Component({
+import { FormsModule } from "@angular/forms";
+import { NgClass } from "@angular/common";
+ @Component({
   selector: "app-pages-note",
   templateUrl: "./note.component.html",
   standalone: true,
-  animations: [fadeInOut]
+  animations: [fadeInOut],
+  imports:[FormsModule, NgClass]
 })
 
 export class noteComponent {
@@ -15,4 +17,19 @@ export class noteComponent {
   public closeModal(){
     this.closeNoteEditor.emit('close note editor');
   }
+
+  maxLength = 280;
+  text = '';
+
+  maxInputLength() {
+    // Apenas uma verificação para garantir que o valor não ultrapasse o limite
+    if (this.text.length > this.maxLength) {
+      this.text = this.text.substring(0, this.maxLength);
+    }
+  }
+
+  get remainingCharacters(): number {
+    return this.maxLength - this.text.length;
+  }
+
 }
